e4216d5f4f186e28d4f08ab896a9637f95740833
Yawn93 <beatrizstaubyn@gmail.com>
2023-09-22 05:04:17 -0400

    added password from mongodb

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:NzMtYmVhdHJpenN0@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;

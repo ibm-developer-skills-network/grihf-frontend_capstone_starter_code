7cd492c704abf4d3f9916e44b6b44fa6c728abf0
oussama-ghandour <oussam92.ing@gmail.com>
2023-11-19 12:11:59 -0500

    add_datapicker_timeslot

--- src/Components/AppointmentForm/AppointmentForm.js
+++ src/Components/AppointmentForm/AppointmentForm.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+
 
 const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
     const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [selectedSlot, setSelectedSlot] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(null);
   
+
+    const timeSlots = ['9:00 AM', '10:00 AM', '11:00 AM', '2:00 PM', '3:00 PM'];
     const handleSlotSelection = (slot) => {
       setSelectedSlot(slot);
     };
@@ -14,6 +21,8 @@ const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
       onSubmit({ name, phoneNumber });
       setName('');
       setPhoneNumber('');
+      setSelectedDate(null); // Reset selectedDate after submission
+      setSelectedSlot(null); // Reset selectedSlot after submission
     };
   
     return (
@@ -40,9 +49,25 @@ const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
         </div>
         <div className="form-group">
          <label>Date of Appointment:</label>
+         <DatePicker
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          dateFormat="dd/MM/yyyy"
+          minDate={new Date()} // Restrict past dates
+          placeholderText="Select a date"
+        />
         </div>
         <div className="form-group">
          <label>Book Time Slot:</label>
+            {/* Time Slot Selector (Dropdown) */}
+        <select value={selectedSlot} onChange={(e) => handleSlotSelection(e.target.value)}>
+          <option value="">Select a time slot</option>
+          {timeSlots.map((slot, index) => (
+            <option key={index} value={slot}>
+              {slot}
+            </option>
+          ))}
+        </select>
         </div>
         <button type="submit">Book Now</button>
       </form>

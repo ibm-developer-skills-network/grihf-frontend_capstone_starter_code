13c3aa25e729dce07ab7f04c58934c1471f27fa5
oussama-ghandour <oussam92.ing@gmail.com>
2023-12-07 16:28:21 -0500

    fix_appointment-form

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -7,6 +7,10 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import "./DoctorCard.css"
 import 'reactjs-popup/dist/index.css';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 
 
@@ -53,24 +57,81 @@ function DoctorCard ({name, speciality, experience, ratings}){
 
 
     return (
-    <div  >
-    
-      <div className="searchpage-container" style={{marginTop:"50px"}}>
-        <div className="search-results-container" style={{display:"flex",alignItem:"row"}}>
-                <div className="doctor-card-container">
+    <div>
+        <div className="searchpage-container" style={{marginTop:"50px"}}>
+            <div className="search-results-container" >
+                <div className="doctor-card-container" style={{display:"flex",alignItem:"row"}}>
                    <div className="doctor-card-details-container">
-                   <div className="doctor-card-details">
-                     <div className="doctor-card-detail-name">{name}</div>
-                     <div className="doctor-card-detail-speciality">{speciality}</div>
-                     <div className="doctor-card-detail-experience">{experience} years experience</div>
-                     <div className="doctor-card-detail-consultationfees">Ratings: {ratings}
+                        <div className="doctor-card-details">
+                            <div className="doctor-card-detail-name">{name}</div>
+                            <div className="doctor-card-detail-speciality">{speciality}</div>
+                            <div className="doctor-card-detail-experience">{experience} years experience</div>
+                        <div className="doctor-card-detail-consultationfees">Ratings: {ratings}
                     </div>
-                   </div>
-        </div>       
-    </div>           
-    </div> 
-    </div>
-    
+                </div>
+                </div>       
+            </div>           
+        </div> 
+        <div className="doctor-card-options-container">
+       <Popup
+          style={{ backgroundColor: '#FFFFFF' }}
+          trigger={
+            <button className={`book-appointment-btn ${appointments.length > 0 ? 'cancel-appointment' : ''}`} style={{width:"25%",marginRight:"70%"}}>
+              {appointments.length > 0 ? (
+                <div>Cancel Appointment</div>
+              ) : (
+                <div>Book Appointment</div>
+              )}
+              <div>No Booking Fee</div>
+            </button>
+          }
+          modal
+          open={showModal}
+          onClose={() => setShowModal(false)}
+        >
+          {(close) => (
+            <div className="doctorbg" style={{ height: '80vh'}}>
+              <div>
+                <div className="doctor-card-profile-image-container">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> </svg> */}
+                {/* <img src={defaultImage} alt="Placeholder" className="doctor-profile-image-placeholder" /> */}
+                </div>
+                <div className="doctor-card-details">
+                  <div className="doctor-card-detail-name">{name}</div>
+                  <div className="doctor-card-detail-speciality">{speciality}</div>
+                  <div className="doctor-card-detail-experience">{experience} years experience</div>
+                  <div className="doctor-card-detail-consultationfees">Ratings: {ratings}</div>
+                </div>
+              </div>
+
+              {appointments.length > 0 ? (
+                <>
+                  <h3 style={{ textAlign: 'center' }}>Appointment Booked!</h3>
+                 
+                  {appointments.map((appointment) => (
+                    <div className="bookedInfo" key={appointment.id}>
+                     
+                     
+                      <Card style={{marginTop:"70px", marginLeft:"140px", width:"450px"}}>
+                            <CardContent>
+                            <Typography variant="h5" gutterBottom>Name: {appointment.name}</Typography>
+                            <Typography variant="h5" gutterBottom>Phone Number: +{appointment.phoneNumber}</Typography>     
+                            <Button onClick={() => handleCancel(appointment.id)} 
+                            variant="contained" color="error" style={{width:"60%",marginLeft:"100px"}}>Cancel Appointment</Button>
+                            </CardContent>
+                       </Card>
+                     
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <AppointmentForm doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} />
+              )}
+            </div>
+          )}
+        </Popup> 
+      </div>
+      </div>
     </div>
     
     )
